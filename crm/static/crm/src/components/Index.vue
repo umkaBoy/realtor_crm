7e74100a7cbab7a512ef4ef8ac8ec74e59c1feee
otherwolf <vpe304@gmail.com>
2021-02-23 00:52:23 +0300

    temp 0.2

--- crm/static/crm/src/components/Index.vue
+++ crm/static/crm/src/components/Index.vue
@@ -64,9 +64,40 @@
                 fab
                 small
                 depressed
-                color="primary"
+                color="transparent"
               >
-                {{ index }}
+                <v-tooltip right>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-img :src="'https://picsum.photos/200/300'" width="40" height="40" v-on="on">
+                      <template v-slot:placeholder>
+                        <v-row
+                          class="fill-height ma-0"
+                          align="center"
+                          justify="center"
+                        >
+                          <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                          ></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                  </template>
+                  <v-img :src="'https://picsum.photos/200/300'" width="250" height="auto">
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="grey lighten-5"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-tooltip>
               </v-btn>
             </v-list-item-action>
 
@@ -130,5 +161,8 @@ export default {
 .col,
 .col-12 {
   padding-top: 8px;
+}
+img {
+  object-fit: cover;
 }
 </style>
